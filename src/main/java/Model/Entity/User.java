d78d9431774c8c96fa550b4f459b604ee20b0f76
JeroenTenorio <jeroen_ralph_tenorio@dlsu.edu.ph>
2025-11-04 16:05:15 +0800

    started user and created userrole

--- src/main/java/Model/Entity/User.java
+++ src/main/java/Model/Entity/User.java
@@ -1,4 +1,17 @@
 package main.java.Model.Entity;
 
+import main.java.Model.Entity.UserRole;
+import java.time.LocalDate;
+
 public class User {
+    private int user_id;
+    private String email;
+    private String passwordHash;
+    private UserRole role;
+    private LocalDate joinDate;
+
+    /**
+     * Default constructor
+     */
+    public User() {}
 }
