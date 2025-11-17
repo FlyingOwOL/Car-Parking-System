259beb46a76f95d2fbf375c8658506c91dabfe1b
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-11-18 02:52:22 +0800

    Update ReservationDAO.java

--- src/main/carpark/DAO/ReservationDAO.java
+++ src/main/carpark/DAO/ReservationDAO.java
@@ -1,5 +1,8 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Optional;
 
 import Model.Entity.Reservation;
@@ -7,11 +10,21 @@ import Model.Entity.Reservation;
 public class ReservationDAO {
     //TODO: SY
 
+    // === SQL QUERRIES === //
+    private static final String SELECT_RESERVATION_BY_ID = "SELECT * FROM reservations WHERE transact_ID = ?";
+    private static final String UPDATE_RESERVATION       = "UPDATE reservations ";
+
+
     public Optional<Reservation> getReservationByID(int reservation_ID){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
         return null;
     }
 
     public boolean updateReservationStatus(int reservation_ID, String STATUS_COMPLETED){
+        
         return false;
     }
 }
