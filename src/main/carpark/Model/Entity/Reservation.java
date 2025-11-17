626b3e4f7943aa6dace5a7453eb40333d23cf996
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-11-17 23:59:04 +0800

    Added getters to Reservation

--- src/main/carpark/Model/Entity/Reservation.java
+++ src/main/carpark/Model/Entity/Reservation.java
@@ -1,9 +1,10 @@
 package Model.Entity;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Reservation {
-    //TODO: SY
+    // SY
     private int reservation_ID;
     private int branchID;
     private SlotType slotType;
@@ -16,26 +17,96 @@ public class Reservation {
     private LocalDateTime checkOutTime;
     private String status;
 
-    public Reservation(){
-        
+    // Make new reservation
+    public Reservation(int branchID, 
+                       SlotType slotType, int vehicle_ID, 
+                       String spot_ID, LocalDateTime checkInTime){
+        this.branchID = branchID;
+        this.slotType = slotType;
+        this.vehicle_ID = vehicle_ID;
+        this.spot_ID = spot_ID;
+        this.checkInTime = checkInTime;
+    }
+    // loading reservation
+    public Reservation(int reservation_ID, int branchID, 
+                       SlotType slotType, int vehicle_ID, 
+                       String spot_ID, LocalDateTime checkInTime, 
+                       LocalDateTime checkOutTime){
+
+        this.reservation_ID = reservation_ID;
+        this.branchID = branchID;
+        this.slotType = slotType;
+        this.vehicle_ID = vehicle_ID;
+        this.spot_ID = spot_ID;
+        this.checkInTime = checkInTime;
+        this.checkOutTime = checkOutTime;
+    }
+    // reservation in advance
+    public Reservation(int reservation_ID, int branchID, 
+                       SlotType slotType, int vehicle_ID, 
+                       String spot_ID, LocalDateTime checkInTime, 
+                       LocalDateTime checkOutTime, boolean isAdvanceReserve,
+                       LocalDateTime expected_time_in, LocalDateTime dateReserved){
+
+        this.reservation_ID = reservation_ID;
+        this.branchID = branchID;
+        this.slotType = slotType;
+        this.vehicle_ID = vehicle_ID;
+        this.spot_ID = spot_ID;
+        this.expected_time_in = expected_time_in;
+        this.dateReserved = dateReserved;
+        this.isAdvanceReserve = isAdvanceReserve;
+        this.checkInTime = checkInTime;
+        this.checkOutTime = checkOutTime;                        
     }
 
     public int getBranchID(){
         return this.branchID;
     }
     public SlotType getSlotType(){
-        return null;
+        return this.slotType;
     }
     public int getID(){
-        return 0;
+        return this.reservation_ID;
+    }
+    public int getVehicleID() {
+        return this.vehicle_ID;
+    }
+    public String getSpotID() {
+        return this.spot_ID;
+    }
+    public LocalDateTime getExpectedTimeIn() {
+        return this.expected_time_in;
+    }
+    public LocalDateTime getDateReserved() {
+        return this.dateReserved;
+    }
+    public boolean isAdvanceReserve() { 
+        return this.isAdvanceReserve;
+    }
+    public String getStatus() {
+        return this.status;
     }
     public LocalDateTime getCheckInTime(){
-        return LocalDateTime.now();
+       if (isAdvanceReserve()){
+            return this.expected_time_in;
+       } else {
+            return this.checkInTime;
+       }
     }
     public LocalDateTime getTimeOut(){
-        return LocalDateTime.now();
+        return this.checkOutTime;
     }
-    public int getReserved_hours(){
-        return 0;
+    public int getReserved_hours() {
+        if (checkInTime != null && checkOutTime != null && checkOutTime.isAfter(checkInTime)) {
+            Duration duration = Duration.between(checkInTime, checkOutTime);
+            return (int) duration.toHours(); 
+        }
+        return 0;  
+    }
+
+    // Use to update reservations
+    public void setCheckOutTime (LocalDateTime checkOut){
+        this.checkOutTime = checkOut;
     }
 }
